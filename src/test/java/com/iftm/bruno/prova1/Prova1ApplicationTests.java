12d23b4ce05118a1af50a11944cc9ef13fd98555
brunovcunha <bruno.cunha@estudante.iftm.edu.br>
2024-08-01 19:39:49 -0300

    iniciando projeto

---
+++ src/test/java/com/iftm/bruno/prova1/Prova1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iftm.bruno.prova1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Prova1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
